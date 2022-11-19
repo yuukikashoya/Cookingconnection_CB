954e657a941ada65e58dac8d52cf3d893ce7b43d
Ponkotsu_Coder <20140943@dwc-legazpi.edu>
2022-11-19 09:49:12 +0800

    test

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,5 +1,7 @@
 import { Sabog } from "./post.model";
 import { Injectable } from "@angular/core";
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 
 @Injectable ({providedIn: 'root'})
 export class PostService{
@@ -7,10 +9,44 @@ export class PostService{
 
 
 
+      constructor(private fireauth:AngularFireAuth, private router:Router) { }
+    
+    
+      login(email:string,password:string){
+        this.fireauth.signInWithEmailAndPassword(email,password).then((res)=>{
+          localStorage.setItem('token','true');
+          this.router.navigate(['/aut'])
+    
+        },err=>{
+          alert(err.message)
+          this.router.navigate(['/login'])
+        })
+      }
+    
+      //signup
+      signup(email:string, password:string)
+      {
+        this.fireauth.createUserWithEmailAndPassword(email,password).then((res)=>{
+          alert("User is now Registered")
+          this.router.navigate(['/aut'])
+        },err=>{
+          alert(err.message)
+          this.router.navigate(['/signup'])
+        })
+      }
+    
+      //logout
+      logout(){
+        this.fireauth.signOut().then(()=>{
+          localStorage.removeItem('token');
+          this.router.navigate(['/login'])
+        },err=>{
+          alert(err.message)
+        })
+      }
+   
    getPost(){
       return this.postList
    }
 }
-
-
 
